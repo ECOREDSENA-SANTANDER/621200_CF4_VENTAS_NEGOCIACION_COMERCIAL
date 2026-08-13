04e5363ba71397730c9a8f5e2716825633ef6de9
lizmanchego <karinamanchego0416@gmail.com>
2026-08-13 09:15:33 -0500

    [ADD] CFA + Material ZIP

--- src/config/global.js
+++ src/config/global.js
@@ -240,7 +240,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CF4_631101_DU.pdf',
+        download: 'downloads/CF4_631101_DU.zip',
       },
       {
         icono: 'fas fa-download',
